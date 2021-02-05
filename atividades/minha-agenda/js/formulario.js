912bdcdb90ad764b39d393676b52cd4bc3ee95ae
Taiane <taiane.taveira@soulasalle.com.br>
2021-02-05 14:09:55 -0300

    desafios com comentarios de estudos

--- atividades/minha-agenda/js/formulario.js
+++ atividades/minha-agenda/js/formulario.js
@@ -19,3 +19,13 @@ formulario.addEventListener('submit', function (event) {
     }
 
 });
+
+// aqui, primeiramente importamos a funçao "adicionarContato" que se encontra em outro arquivo. Criamos então a constante formulario para 
+// receber a referencia do form que contem a classe "row" na dom. Adicionamos a constante um evento escutador de submit (lembrando que submit 
+// acontece por padrão ao clicar um botao dentro de um form) e iniciamos a função callback. Adicionamos como primeira regra a função de prevenir 
+// o comportamento padrao do evento, que é de recarregar a página ao clicar o botão. Posteriormente criamos uma constante "nome" e outra "telefone"
+// para receberem respectivamente as referencias nos inputs na DOM destinados a nome e delefone. Abrimos uma estrutura do tipo if na qual se o valor do
+// da variavel nome menos os espaços em branco (trim()) forem falsos criamos um alerta que busca a mensagem configurada no dataset da dom. Realizamos
+// o mesmo procedimento para o campo telefone na DOM, na continuação da estrutura (else if). Para o caso contrato (else - se ambos tiverem valores 
+// verdadeiros) chamamos a função adicionarContato com parametros do valor inputado no campo nome, e valor inputado no campo telefone. Por ultimo
+// pedimos para resetar o formulário, para que depois de enviados os dados os campos ficarem novamente em branco. 
--- atividades/minha-agenda/js/tabela-contatos.js
+++ atividades/minha-agenda/js/tabela-contatos.js
@@ -21,3 +21,12 @@ tabela.addEventListener('click', function (event) {
         botaoExcluir.closest('tr').remove();
     }
 });
+
+// primeiro criamos a constante tabela para receber a referencia do elemento que contem o id tabelaContatos. Então criamos uma função exportavel (para 
+// que possa ser chamada em outro documento) que recebe os parametros nome e telefone. Dentro da função criamos a função novaLinha que recebe 
+// a criação de um elemento tr na DOM. A ela atribuimos a propriedade innerHTML na qual configuramos os elementos html e as variáveis que são os para 
+// metros da função. Pedimos que à variavel tabela seja adicionada a novaLinha.
+// Por ultimo, fora da função, adicionamos um evento escutador de flic a tabela. Abrimos uma estrutura do tipo if que verifica se o alvo do evento  
+// contem a classe btn-danger. Em caso positivo criamso uma constante botaoExcluir que recebe o alvo do evento. Adicionamos a ela então um remove
+// para o elemento mais proximo que seja uma tr. Isso pq o botaoExcluir é um button que está dentro de uma tr, e queremos que ao ser acionado, ele  
+// remova toda a linha(tr) a que pertence.
